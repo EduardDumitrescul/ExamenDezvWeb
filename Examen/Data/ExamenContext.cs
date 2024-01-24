d92cbde25ffbc1435aa7f1d161f0a94a45f25102
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2024-01-24 15:29:17 +0200

    setup examen

---
+++ Examen/Data/ExamenContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Examen.Data
+{
+    public class ExamenContext: DbContext
+    {
+        public ExamenContext(DbContextOptions<ExamenContext> options) : base(options) { }
+    }
+}
